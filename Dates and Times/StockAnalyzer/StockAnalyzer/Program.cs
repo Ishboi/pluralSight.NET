bfb5ef7cb8f7457635e3c0b4254ec5f9184a2f20
BuildTools <bolachaseleite@gmail.com>
2020-11-14 15:02:43 +0000

    Date and time fundamentals, handling date & time abiguity, working with time zones. Why DateTimeOffset is superior to DateTime. It is superior due to the fact that datetimeoffset gives the offset to UTC. Formatting and parsing different dates. How UTC makes everyones life easier. Use utc instead of localtime, avoiding ambiguity

---
+++ Dates and Times/StockAnalyzer/StockAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace StockAnalyzer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var now = DateTimeOffset.UtcNow;
+
+      Console.WriteLine(now.ToLocalTime());
+
+    }
+  }
+}
